36fad92ea87a65fccda3410a5d939cc1e7266fbb
SaeedShariati <global.arrive2017@gmail.com>
2025-12-19 11:02:45 +0330

    removed unnecessary code

--- Process_API/Ex3_fork_order.c
+++ Process_API/Ex3_fork_order.c
@@ -5,8 +5,6 @@
 #include<fcntl.h>
 #include<string.h>
 int main(int argc, char* argv[]){
-    int fd = open("./test.txt",O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
-    int t = 100;
     int rc = fork();
 
     if(rc==0)//child process
--- Process_API/Ex4_fork_exec.c
+++ Process_API/Ex4_fork_exec.c
@@ -5,7 +5,6 @@
 #include<fcntl.h>
 #include<string.h>
 int main(int argc, char* argv[]){
-    int fd = open("./test.txt",O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
     int rc = fork();
     if(rc==0)//child process
     {
